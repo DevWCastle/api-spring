393998d7b011a7f841da6115c478e5d9fec39536
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-10-10 22:48:17 -0300

    Adicionando um controller com uma rota simples de teste

---
+++ src/main/java/DevWCastle/api_spring/Controller.java
@@ -0,0 +1,15 @@
+package DevWCastle.api_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController// definindo que a classe é um controller rest
+public class Controller {
+
+    @GetMapping("/home") //Anotação para definir rotas gets
+    public String home(){
+        return  "Tudo na paz tropa";
+    }
+
+
+}
