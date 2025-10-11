1a5c2cfae37f43fbc213c4c6c3953d7d3c0509ea
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-10-11 12:28:21 -0300

    Renomeando Controller

--- src/main/java/DevWCastle/api_spring/ClientController.java
+++ src/main/java/DevWCastle/api_spring/ClientController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController// definindo que a classe é um controller rest
-public class Controller {
+public class ClientController {
 
     @GetMapping("/home") //Anotação para definir rotas gets
     public String home(){
