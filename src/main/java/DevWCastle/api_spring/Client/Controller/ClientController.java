cca5558da3670a4dc293b19773b07a2e94c087d6
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-10-18 23:34:43 -0300

    Adicionando 3 endpoints com seus respectivos parametros e metodos e assinaturas

---
+++ src/main/java/DevWCastle/api_spring/Client/Controller/ClientController.java
@@ -0,0 +1,39 @@
+package DevWCastle.api_spring.Client.Controller;
+
+import DevWCastle.api_spring.Client.Model.ClientModel;
+import DevWCastle.api_spring.Client.Service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@RestController// definindo que a classe é um controller rest
+@RequestMapping("/clientes")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    //Autowired faz o negocio abaixo de injeção de dependencia
+    public ClientController(ClientService clientService){
+       this.clientService = clientService;
+    }
+
+    @GetMapping
+    public List<ClientModel> getAll(){return this.clientService.getAll();}
+
+    @GetMapping(value="/{id}")//ja considera o /clientes do controller então: /clientes/{id}
+    public Optional<ClientModel> getById(@PathVariable Long id){ return this.clientService.getById(id);}
+
+    @PostMapping
+    public ClientModel create(@RequestBody ClientModel clientModel){return this.clientService.createClient(clientModel);}
+
+    @PutMapping(value="/atualizar/{id}")
+    public Optional<ClientModel> update(@RequestBody ClientModel clientModel, @PathVariable Long id){
+        return this.clientService.updateClient(clientModel, id);
+    }
+
+
+
+}
